69964698a36a52a1d3df31b48193e08d367f85b4
SiegfriedWagner <mateus.chojnowski@gmail.com>
2018-11-14 06:50:36 +0100

    auctex support

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -1,7 +1,7 @@
 (require 'package)
 (require 'company)
-;; (load "auctex.el" nil t t)
-;; (load "preview-latex.el" nil t t)
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
 (use-package tex
   :defer t
   :ensure auctex
@@ -10,5 +10,7 @@
 (use-package company-auctex
   :ensure t
   :config
+  (require 'company-auctex)
   (company-auctex-init))
+(add-hook 'latex-mode 'visual-line-mode)
 (provide 'init-tex)
